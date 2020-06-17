b2ec80ca308ffa321dd881c5884a579a74ef196d
Amy Barrett <a.d.barrett@outlook.com>
2020-06-17 12:00:32 +0100

    Initial commit

---
+++ CustomValidationDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CustomValidationDemo.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
