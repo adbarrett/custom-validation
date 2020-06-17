fa27b5a763781a6fa9ed13bc52f25b03b9dac77f
Amy Barrett <a.d.barrett@outlook.com>
2020-06-17 12:06:27 +0100

    Removed privacy page

--- CustomValidationDemo/Controllers/HomeController.cs
+++ CustomValidationDemo/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CustomValidationDemo.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using CustomValidationDemo.Models;
+using System.Diagnostics;
 
 namespace CustomValidationDemo.Controllers
 {
@@ -19,11 +15,6 @@ namespace CustomValidationDemo.Controllers
 		}
 
 		public IActionResult Index()
-		{
-			return View();
-		}
-
-		public IActionResult Privacy()
 		{
 			return View();
 		}
